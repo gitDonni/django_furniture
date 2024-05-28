9233cf90eff408703ecf78300fc3c1a890e46a70
daniyar <asankadyrov.daniyar@gmail.com>
2024-05-28 14:10:13 +0600

    added fixtures to products and cats

--- main/views.py
+++ main/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from products.models import Categories
+
 
 def index(request):
+    categories = Categories.objects.all()
     context = {
         'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME'
+        'content': 'Магазин мебели HOME',
+        'categories': categories
     }
     return render(request, 'main/index.html', context)
 
