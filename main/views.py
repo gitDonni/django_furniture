715a070779c0e7e0525780e4d6a60503ac7bf930
daniyar <asankadyrov.daniyar@gmail.com>
2024-05-29 22:48:43 +0600

    Registered simple tag for categories

--- main/views.py
+++ main/views.py
@@ -4,11 +4,9 @@ from products.models import Categories
 
 
 def index(request):
-    categories = Categories.objects.all()
     context = {
         'title': 'Home - Главная',
         'content': 'Магазин мебели HOME',
-        'categories': categories
     }
     return render(request, 'main/index.html', context)
 
